6b282c4e1ea5b82d2694c1be407894e05704ae4c
CristianFortich <yezidfortich@gmail.com>
2022-08-30 20:08:40 -0500

    a little fix to the landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,10 +6,11 @@ import BG from '../../pics/MB.jpg'
 const LandingPage = (props) => {
     const history = useHistory()
     return (
-    <div style={{ backgroundImage: `url(${BG})` }} className={lpStyle.mainBox}>
-      <p aling='center'>
-        <button onClick={()=>history.push("/home")} className={lpStyle.enterButton}>Enter</button>
-      </p>
+      <div style={{ backgroundImage: `url(${BG})` }} className={lpStyle.mainBox}>
+        <div className={lpStyle.header}></div>
+        <p className={lpStyle.p}>
+          <button onClick={()=>history.push("/home")} className={lpStyle.enterButton}>Enter</button>
+        </p>
     </div>
   )
 }
